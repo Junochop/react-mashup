c046e0e23d59b85bbdaea210ed7c8019e30e03ed
Juno Lee <lejuno80@gmail.com>
2018-06-27 20:46:04 -0500

    returning data set

--- src/app/App.js
+++ src/app/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 
 import Animals from '../components/Animals/Animals.js';
 
+import connection from '../firebaseRequests/connection';
+
+import mashupRequests from '../firebaseRequests/animals';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    mashups: [],
+  }
+
+  componentDidMount () {
+    // connection(fb);
+    connection();
+
+    mashupRequests.getRequest()
+      .then((mashups) => {
+        this.setState({ mashups});
+      })
+      .catch((err) => {
+        console.error('error', err);
+      });
+  };
 
   render () {
     return (
       <div className="App">
         <div className="col-sm-6">
-          <Animals />
+          <Animals mashups={this.state.mashups} />
         </div>
       </div>
     );

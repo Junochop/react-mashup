c5e5e160e992a974509bafd27db0a2c7dc90451b
Juno Lee <lejuno80@gmail.com>
2018-06-29 18:02:43 -0500

    add thumbnails

--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -7,14 +7,14 @@ class Animals extends React.Component {
     const {mashups} = this.props;
     const animalsItemComponents = mashups.map((item) => {
       return (
-        <div key={item.id} class='img-thumbnail'>{item.name}
-          <img class='animal-image' src={item.imgUrl} alt="card"/>
+        <div key={item.id} className='img-thumbnail Animals'>{item.name}
+          <img className='animal-image' src={item.imgUrl} alt="card"/>
           <p>{item.description}</p>
         </div>
       );
     });
     return (
-      <div className="Animals">
+      <div className="col-sm-6 col-md-4">
         <h2>Animal</h2>
         <ul>
           {animalsItemComponents}
